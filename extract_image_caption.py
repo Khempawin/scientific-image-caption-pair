484a8825a6d555a6e8833cf013b2c7b60123c029
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-07-26 00:09:23 -0600

    add output caption file type option [csv | parquet]

--- extract_image_caption.py
+++ extract_image_caption.py
@@ -5,6 +5,7 @@ import os
 import xml.etree.ElementTree as ET
 import tarfile
 import argparse
+import pandas as pd
 from pathlib import Path
 from time import time
 from concurrent.futures import ProcessPoolExecutor
@@ -225,7 +226,8 @@ def process_tar_dir(target_dir:str,
                     first_level_code:str, 
                     second_level_code:str, 
                     flatten_output_dir=False, 
-                    omit_image_file:bool=True):
+                    omit_image_file:bool=True,
+                    output_caption_file_type: str="csv"):
     logger = logging.getLogger(__name__)
     start_time = time()
     documents = os.scandir(target_dir)
@@ -254,11 +256,16 @@ def process_tar_dir(target_dir:str,
                                               omit_image_file=omit_image_file)
         record_list.extend(subrecord_list)
 
-    csv_file_path = output_dir_base / f"{first_level_code}_{second_level_code}_captions.csv" if flatten_output_dir else output_dir_base / "captions.csv"
+    if(output_caption_file_type == "parquet"):
+        record_df = pd.DataFrame(record_list)
+        parquet_path = output_dir_base / f"{first_level_code}_{second_level_code}_captions.parquet.gzip" if flatten_output_dir else output_dir_base / "captions.parquet.gzip"
+        record_df.to_parquet(parquet_path, compression="gzip")
+    else:
+        csv_file_path = output_dir_base / f"{first_level_code}_{second_level_code}_captions.csv" if flatten_output_dir else output_dir_base / "captions.csv"
 
-    save_records_to_csv(csv_file_path, 
-                        record_list, 
-                        sep="|")
+        save_records_to_csv(csv_file_path, 
+                            record_list, 
+                            sep="|")
     end_time = time()
     logger.info("  Time for completion of {}/{}: {:.2f} seconds containing {}".format(first_level_code, second_level_code, (end_time-start_time), len(record_list)))
     return record_list
@@ -269,7 +276,8 @@ def extract_all(main_dir: str,
                 n_workers=1, 
                 log_level=logging.INFO, 
                 flatten_output_dir=False, 
-                omit_image_file:bool=True):
+                omit_image_file:bool=True,
+                output_caption_file_type: str="csv"):
     output_dir = Path(output_path)
     output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -290,7 +298,8 @@ def extract_all(main_dir: str,
                              first_level_dir.name,
                              second_level_dir.name,
                              flatten_output_dir,
-                             omit_image_file) for second_level_dir in second_level]
+                             omit_image_file,
+                             output_caption_file_type) for second_level_dir in second_level]
 
 
 def parse_log_level(level: str):
@@ -318,6 +327,7 @@ Image-Caption pair extraction from scientific papers
     parser.add_argument("-n", "--n_workers", help="number of worker processes", default=1)
     parser.add_argument("-l", "--log_level", help="log level [0: debug, 1: info (normal operation)]", default="info")
     parser.add_argument("--omit_image_file", help="Omit image files [True | False]", default=False)
+    parser.add_argument("--caption_output_type", help="Specify output caption file format [csv | parquet]", default="csv")
     arg_list = parser.parse_args()
     main_dir = arg_list.input_dir
     output_path = arg_list.output_dir
@@ -325,12 +335,14 @@ Image-Caption pair extraction from scientific papers
     n_workers = int(arg_list.n_workers)
     log_level = parse_log_level(arg_list.log_level)
     omit_image_file = parse_boolean(arg_list.omit_image_file)
+    output_caption_file_type = arg_list.caption_output_type
     extract_all(main_dir=main_dir,
                 output_path=output_path,
                 n_workers=n_workers,
                 log_level=log_level,
                 flatten_output_dir=flatten_output_directory,
-                omit_image_file=omit_image_file)
+                omit_image_file=omit_image_file,
+                output_caption_file_type=output_caption_file_type)
 
 
 if __name__ == "__main__":

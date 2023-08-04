dbeb5cf009ac0cc5565f11d12ed786c7a3bc39a0
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-08-04 14:04:19 -0600

    transform images to tensor and load error image

--- ScientificImageCaption/Dataset.py
+++ ScientificImageCaption/Dataset.py
@@ -1,15 +1,13 @@
 import torch
 import pandas as pd
-from PIL import Image
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 from torch.utils.data import Dataset
-from typing import TypedDict
+from torchvision import transforms
+from typing import Tuple
 
 VALID_FIELDS = {"caption", "document_id", "image_path", "image_type", "fit_context", "first_level_dir", "second_level_dir"}
 
-class ImageCaptionDict(TypedDict):
-    image: torch.Tensor | Image.Image
-    caption: str
-    document_id: str
 
 class ScientificImageCaptionDataset(Dataset):
     def get_full_path(
@@ -27,6 +25,13 @@ class ScientificImageCaptionDataset(Dataset):
         self.manifest = pd.read_parquet(parquet_file_path, engine="pyarrow")
         self.image_root_dir = image_root_dir
         self.transform = transform
+        self.default_img_transform = transforms.Compose([
+            transforms.Resize(size=(224,224)),
+            transforms.ToTensor()
+        ])
+        self.expand_img_channel = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3)            
+        ])
 
     def __len__(self) -> int:
         return self.manifest.shape[0]
@@ -40,7 +45,7 @@ class ScientificImageCaptionDataset(Dataset):
             second_level_dir=record["second_level_dir"],
             image_root=self.image_root_dir
         )
-        return Image.open(image_path)
+        return Image.open(image_path).convert("RGB")
     
     def get_field(self, index: int, field_name: str) -> str:
         record = self.manifest.iloc[index]
@@ -50,17 +55,22 @@ class ScientificImageCaptionDataset(Dataset):
 
         return record[field_name]
     
-    def __getitem__(self, index) -> ImageCaptionDict:
-        image = self.load_image(index)
+    def __getitem__(self, index) -> Tuple[torch.Tensor , str, str]:
+        try:
+            record = self.manifest.iloc[index]
+            image_path = self.get_full_path(
+                image_path=record["image_path"], 
+                first_level_dir=record["first_level_dir"], 
+                second_level_dir=record["second_level_dir"],
+                image_root=self.image_root_dir
+            )
+            image = self.load_image(index)
+        except:
+            raise Exception(f"Error read image path : {image_path}")
         caption = self.get_field(index, "caption")
         document_id = self.get_field(index, "document_id")
 
-        if self.transform:
-            image = self.transform(image)
+        image = self.transform(image) if self.transform else self.default_img_transform(image)
 
-        return {
-            "image": image, 
-            "caption": caption, 
-            "document_id": document_id
-        }
+        return (image, caption, document_id)
     

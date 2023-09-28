fd981f6ecb716a63b0c3b84f5c2203e205f27bec
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-09-28 17:49:58 -0600

    Partial work

--- batch-scripts/__03a_encode_captions/encode_captions.py
+++ batch-scripts/__03a_encode_captions/encode_captions.py
@@ -1,6 +1,19 @@
-import dask.dataframe as dd
+import pandas as pd
 import argparse
+import numpy as np
 from pathlib import Path
+from transformers import (
+    VisionTextDualEncoderProcessor,
+    AutoModel,
+    AutoTokenizer,
+    AutoImageProcessor,
+)
+from PIL import Image
+
+# Used to enable VisionTextDualModel encoding of Text
+DUMMY_IMAGE = Image.fromarray(np.random.randint(255, size=(224,224,3),dtype=np.uint8))
+
+
 
 
 def parse_boolean(value: str):
@@ -11,7 +24,7 @@ def parse_boolean(value: str):
 
 def main():
     parser = argparse.ArgumentParser(description="""
-            Select caption records with context length limit 77 and image file exists
+            Encode captions
         """)
     parser.add_argument("-s2", "--stage_2_dir",
                         help="directory output of stage 02, accessible via network by every node", required=True)
@@ -22,7 +35,8 @@ def main():
     parquet_dir = f"{args.stage_2_dir}/captions"
     output_dir = args.output_dir
 
-    records = dd.read_parquet(
+    # Load data manifest
+    records = pd.read_parquet(
         parquet_dir,
         engine="pyarrow",
         columns=[
@@ -40,7 +54,8 @@ def main():
     output_path = Path(output_dir)
     output_path.mkdir(parents=True, exist_ok=True)
 
-
+    print(records.shape)
+    print(records.head())
     # selected.compute().to_parquet(output_path / "captions",
     #                               engine="pyarrow", partition_cols="first_level_dir")
 

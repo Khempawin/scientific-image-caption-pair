aba70fba165af35763f06ca51b46a150ad2ce8aa
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-10-09 00:56:31 -0600

    Breakdown output by first level dir

--- batch-scripts/__03a_encode_captions/encode_captions.py
+++ batch-scripts/__03a_encode_captions/encode_captions.py
@@ -12,6 +12,7 @@ from PIL import Image
 from typing import Tuple
 import numpy as np
 import torch
+import time
 
 # Used to enable VisionTextDualModel encoding of Text
 DUMMY_IMAGE = Image.fromarray(np.random.randint(255, size=(224,224,3),dtype=np.uint8))
@@ -36,6 +37,57 @@ def parse_boolean(value: str):
         return True
     return False
 
+
+def encode_section(
+        records: pd.DataFrame, 
+        first_level_dir: str, 
+        model: AutoModel,
+        processor: VisionTextDualEncoderProcessor,
+        batch_size: int,
+        device: str,
+        image_root_dir: str,
+        output_path: str
+    ):
+    DATA_SIZE = records.shape[0]
+
+    # Divide records into batches
+    batches = [list(range(i, i+batch_size)) if i+batch_size < DATA_SIZE else list(range(i, DATA_SIZE)) for i in range(0, DATA_SIZE, batch_size)]
+
+    # Initialize encoded lists
+    encoded_caption_list = list()
+    encoded_image_list = list()
+    load_status_list = list()
+
+    # Process each batch
+    for batch in batches:
+        image_list = list()
+        # Load images
+        for i in batch:
+            success, image = load_image(get_full_image_path(records.iloc[i], image_root_dir))
+            load_status_list.append(success)
+            image_list.append(image)
+        # Select captions for batch
+        caption_list = list(records.iloc[batch[0]:batch[-1]+1]["caption"])
+        # Encode image and caption
+        with torch.no_grad():
+            inputs = processor(text=caption_list, images=image_list, return_tensors="pt", padding=True)
+            inputs = inputs.to(device)
+            outputs = model(**inputs)
+        
+        # Add encoded batch result to encoded lists
+        encoded_caption_list.extend(list(np.asarray(outputs.text_embeds.to("cpu"))))
+        encoded_image_list.extend(list(np.asarray(outputs.image_embeds.to("cpu"))))
+
+    # Assign encoded results to original dataframe
+    records["encoded_caption"] = encoded_caption_list
+    records["encoded_image"] = encoded_image_list
+    records["load_status"] = load_status_list
+
+    # Save dataframe to parquet file
+    valid_records = records[records["load_status"] == True]
+    print("Processed records of {} : {} | Valid records : {}".format(first_level_dir, records.shape[0], valid_records.shape[0]))
+    valid_records.to_parquet(output_path / f"encoded_caption_{first_level_dir}.parquet", engine="pyarrow")
+    return
 
 def main():
     parser = argparse.ArgumentParser(description="""
@@ -95,44 +147,23 @@ def main():
     # Select data based on data size
     records = records[records["original_index"] < DATA_SIZE]
 
-    # Divide records into batches
-    batches = [list(range(i, i+BATCH_SIZE)) if i+BATCH_SIZE < DATA_SIZE else list(range(i, DATA_SIZE)) for i in range(0, DATA_SIZE, BATCH_SIZE)]
+    letters = "0123456789abcdef"
+    first_level_dir_list = ["{}{}".format(i, j) for i in letters for j in letters]
 
-    # Initialize encoded lists
-    encoded_caption_list = list()
-    encoded_image_list = list()
-    load_status_list = list()
-
-    # Process each batch
-    for batch in batches:
-        image_list = list()
-        # Load images
-        for i in batch:
-            success, image = load_image(get_full_image_path(records.iloc[i], image_root_dir))
-            load_status_list.append(success)
-            image_list.append(image)
-        # Select captions for batch
-        caption_list = list(records.iloc[batch[0]:batch[-1]+1]["caption"])
-        # Encode image and caption
-        with torch.no_grad():
-            inputs = processor(text=caption_list, images=image_list, return_tensors="pt", padding=True)
-            inputs = inputs.to(device)
-            outputs = model(**inputs)
-        
-        # Add encoded batch result to encoded lists
-        encoded_caption_list.extend(list(np.asarray(outputs.text_embeds.to("cpu"))))
-        encoded_image_list.extend(list(np.asarray(outputs.image_embeds.to("cpu"))))
-
-    # Assign encoded results to original dataframe
-    records["encoded_caption"] = encoded_caption_list
-    records["encoded_image"] = encoded_image_list
-    records["load_status"] = load_status_list
-
-    # Save dataframe to parquet file
-    valid_records = records[records["load_status"] == True]
-    print("Processed records : {} | Valid records : {}".format(records.shape[0], valid_records.shape[0]))
-    valid_records.to_parquet(output_path / "encoded_caption.parquet", engine="pyarrow")
-
+    for first_level_dir in first_level_dir_list:
+        start_time = time.time()
+        encode_section(
+            records[records["first_level_dir"] == first_level_dir],
+            first_level_dir,
+            model,
+            processor,
+            BATCH_SIZE,
+            device,
+            image_root_dir,
+            output_path
+        )
+        print("{} : ---{:.2f}--- seconds".format(first_level_dir, time.time() - start_time))
+    
 
 if __name__ == "__main__":
     main()

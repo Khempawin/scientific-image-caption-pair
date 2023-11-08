de6987f953339999a8dfd5e29ef5c3a4fe9a85a8
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-11-08 12:31:25 -0700

    Save images to zip file

--- batch-scripts/__00_extract_image_caption/extract_image_caption.py
+++ batch-scripts/__00_extract_image_caption/extract_image_caption.py
@@ -14,6 +14,7 @@ from xml.etree.ElementTree import Element
 from typing import List, Dict, Any
 from tarfile import TarFile
 from logging import Logger
+from zipfile import ZipFile
 
 
 def extract_directory(target_root:str) -> List[DirEntry]:
@@ -50,7 +51,15 @@ def process_caption(node: Element):
     return "{}{}".format(caption_title, caption_p)
 
 
-def process_graphic(tar_archive: TarFile, node: Element, first_level_code: str, second_level_code: str, document_id: str, output_image_dir: str, logger: Logger=None, omit_image_file: bool=True):
+def process_graphic(
+        tar_archive: TarFile, 
+        node: Element, 
+        first_level_code: str, 
+        second_level_code: str, 
+        document_id: str, 
+        output_image_zip: ZipFile=None, 
+        logger: Logger=None, 
+        omit_image_file: bool=True):
     if logger is None:
         logger = logging
     IMAGE_FILE_EXTENSIONS = [".jpg", ".png", ".gif", ".tif"]
@@ -87,9 +96,8 @@ def process_graphic(tar_archive: TarFile, node: Element, first_level_code: str, 
 
     image_file = tar_archive.extractfile("{}/{}".format(document_id, image_name))
     
-    # Save image to output directory
-    with open(f"{output_image_dir}/{saved_image_name}", "wb") as f:
-        f.writelines(image_file.readlines())
+    # Save image to output zip
+    output_image_zip.writestr(saved_image_name, image_file.read())
 
     return saved_image_name
 
@@ -107,7 +115,7 @@ def process_node_with_graphic(tar_archive: TarFile,
                               first_level_code: str, 
                               second_level_code: str, 
                               document_id: str, 
-                              output_image_dir: str, 
+                              output_image_zip: ZipFile=None, 
                               logger: Logger=None, 
                               omit_image_file:bool=True):
     if logger is None:
@@ -125,7 +133,7 @@ def process_node_with_graphic(tar_archive: TarFile,
                                  first_level_code=first_level_code,
                                  second_level_code=second_level_code,
                                  document_id=document_id, 
-                                 output_image_dir=output_image_dir, 
+                                 output_image_zip=output_image_zip, 
                                  logger=logger,
                                  omit_image_file=omit_image_file)
 
@@ -150,7 +158,7 @@ def get_section_title(section_node: Element):
 def process_document_tar(entry: DirEntry, 
                          first_level_code:str, 
                          second_level_code:str, 
-                         output_image_dir: str="output/images", 
+                         output_image_zip: ZipFile=None, 
                          logger: Logger=None, 
                          omit_image_file:bool=True):
     if logger is None:
@@ -196,13 +204,8 @@ def process_document_tar(entry: DirEntry,
         figure_nodes = list()
         for section_node in section_nodes:
             section_title = get_section_title(section_node)
-            # print(section_title)
             sub_figure_nodes = [{"node": node, "section": section_title} for node in section_node.iter() if node_has_graphic(node)]
             figure_nodes.extend(sub_figure_nodes)
-        # print(nxml_file_name, len(figure_nodes))
-        # for figure_dict in figure_nodes:
-        #     print(f"{figure_dict.get('section')} : {figure_dict.get('node')}")
-        # print("", end="\n\n")
 
         if len(figure_nodes) == 0:
             return []
@@ -213,7 +216,7 @@ def process_document_tar(entry: DirEntry,
                                                 first_level_code=first_level_code,
                                                 second_level_code=second_level_code,
                                                 document_id=document_id, 
-                                                output_image_dir=output_image_dir,
+                                                output_image_zip=output_image_zip,
                                                 omit_image_file=omit_image_file) for figure in figure_nodes]
         record_list = list(filter(lambda x: x is not None, record_list))
         
@@ -247,10 +250,13 @@ def process_tar_dir(target_dir: str,
     output_dir_base = Path(f"{output_dir_prefix}/output_{output_dir_suffix}") if flatten_output_dir else Path(
         f"{output_dir_prefix}/output_{first_level_code}")
     output_dir_base.mkdir(parents=True, exist_ok=True)
+        
+    output_image_zip = None
     output_image_dir = Path(f"{output_dir_prefix}/output_{output_dir_suffix}/images") if flatten_output_dir else Path(
         f"{output_dir_prefix}/output_{first_level_code}/{first_level_code}_{second_level_code}_images")
     if(not omit_image_file):
-        output_image_dir.mkdir(parents=True, exist_ok=True)
+        output_image_zip = ZipFile(f"{output_image_dir}.zip", "w")
+
 
     record_list = list()
 
@@ -260,11 +266,13 @@ def process_tar_dir(target_dir: str,
         subrecord_list = process_document_tar(doc,
                                               first_level_code=first_level_code,
                                               second_level_code=second_level_code,
-                                              output_image_dir=output_image_dir,
+                                              output_image_zip=output_image_zip,
                                               omit_image_file=omit_image_file)
         record_list.extend(subrecord_list)
 
     record_df = pd.DataFrame(record_list)
+    if(output_image_zip is not None and not omit_image_file):
+        output_image_zip.close()
     if(output_caption_file_type == "parquet"):
         parquet_path = output_dir_base / \
             "captions.parquet" if flatten_output_dir else output_dir_base / \
